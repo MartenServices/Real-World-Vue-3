284617feb094b7aff5753c943d29e6ebba52c3c0
MartenServices <r.p.v.marten@gmail.com>
2021-09-16 23:21:49 +0200

    add axios pull api req

---
+++ real-world-vue-3/src/views/EventList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="events">
+    <EventCard />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+// import axios from "axios";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data(){
+    // events: null
+  },
+};
+</script>
